75280e0d8f3e106a955577b19256485971d7068f
denislets <denislets@gmail.com>
2014-08-27 23:03:59 +0400

    Удаление ненужных файлов

--- perBot.php
+++ perBot.php
@@ -13,11 +13,8 @@ include('botCore/cUrlClass.php');
 include('botCore/simple_html_dom.php');
 include('botCore/json.php');
 include('botCore/xml.php');
-include('botCore/links.php');
 include('botCore/perUtils.class.php');
 include('botCore/perBot.class.php');
 include('botCore/perInfo.class.php');
-include('botCore/perMail.class.php');
-include('botCore/perBattle.class.php');
 
 $html = new simple_html_dom;
